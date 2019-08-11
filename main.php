6184212628ce821a41daef303a322cb12532179b
Piotr <dylag.piotr@gmail.com>
2019-08-11 11:29:58 +0200

    Initial commit

---
+++ main.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<link rel="icon" href="image/metroexodusicon.png">
+<link rel="stylesheet" href="css/style.css">
+<link rel="script" href="js/">
+<meta charset="UTF-8">
+<title>Uniwersum Metro</title>
+<link href='https://fonts.googleapis.com/css?family=Rubik Mono One' rel='stylesheet'>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="menu">
+    <nav>
+        <ul>
+            <li class="menu"> <a href="#"> GŁÓWNA</a></li>
+            |
+            <li class="menu"> <a href="#"> O UNIWERSUM </a></li>
+            |
+            <li class="menu"><a href="#">KSIĄŻKI </a></li>
+            |
+            <li class="menu"><a href="#">GRY</a></li>
+            |
+        </ul>
+    </nav>
+</div>
+</body>
+
+
+</html>
+
