b90b097081bda2d31de0463aaf79527dd64e0e31
Piotr <dylag.piotr@gmail.com>
2019-08-27 16:55:02 +0200

    setup slider

--- main.php
+++ main.php
@@ -56,27 +56,12 @@
                     <a class="dropdown-item" href="#">Metro Exodus</a>
                 </div>
             </li>
-            <a class="login-trigger" href="#" data-target="#login" data-toggle="modal">Login</a>
 
-            <div id="login" class="modal fade" role="dialog">
-                <div class="modal-dialog">
 
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <button data-dismiss="modal" class="close">&times;</button>
-                            <h4>Login</h4>
-                            <form>
-                                <input type="text" name="username" class="username form-control" placeholder="Username"/>
-                                <input type="password" name="password" class="password form-control" placeholder="password"/>
-                                <input class="btn login" type="submit" value="Login" />
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </ul>
     </div>
 </nav>
+
 <div id="slider" class="carousel slide carousel-fade" data-ride="carousel">
     <!--Indicators-->
     <ol class="carousel-indicators">
