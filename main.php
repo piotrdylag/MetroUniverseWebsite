51caf99765d84b7a14ae8c38b14a5f38cd36f74d
Piotr Dylag <dylag.piotr@gmail.com>
2019-08-27 15:15:45 +0200

    add carusele

--- main.php
+++ main.php
@@ -2,8 +2,7 @@
 <html lang="pl">
 <head>
 <link rel="icon" href="image/metroexodusicon.png">
-<link rel="stylesheet" href="css/style.css">
-<link rel="script" href="js/">
+<link rel="stylesheet" href="style/style.css">
 <meta charset="UTF-8"><meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <title>Uniwersum Metro</title>
@@ -57,44 +56,68 @@
                     <a class="dropdown-item" href="#">Metro Exodus</a>
                 </div>
             </li>
+            <a class="login-trigger" href="#" data-target="#login" data-toggle="modal">Login</a>
+
+            <div id="login" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <button data-dismiss="modal" class="close">&times;</button>
+                            <h4>Login</h4>
+                            <form>
+                                <input type="text" name="username" class="username form-control" placeholder="Username"/>
+                                <input type="password" name="password" class="password form-control" placeholder="password"/>
+                                <input class="btn login" type="submit" value="Login" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </ul>
     </div>
 </nav>
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
+<div id="slider" class="carousel slide carousel-fade" data-ride="carousel">
+    <!--Indicators-->
     <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#slider" data-slide-to="0" class="active"></li>
+        <li data-target="#slider" data-slide-to="1"></li>
+        <li data-target="#slider" data-slide-to="2"></li>
     </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="image/Metro2033_foto.jpg" alt="Metro 2033" style="width:100%;">
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+        <!--First slide-->
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="image/Metro2033_foto.jpg"
+                 alt="First slide">
         </div>
-
-        <div class="item">
-            <img src="image/Metro2033_foto2.jpg" alt="Metro 2033 #2" style="width:100%;">
+        <!--/First slide-->
+        <!--Second slide-->
+        <div class="carousel-item">
+            <img class="d-block w-100" src="image/Metro2033_foto2.jpg"
+                 alt="Second slide">
         </div>
-
-        <div class="item">
-            <img src="image/Metro2033_foto3.jpg" alt="Metro 2033 #3" style="width:100%;">
+        <!--/Second slide-->
+        <!--Third slide-->
+        <div class="carousel-item">
+            <img class="d-block w-100" src="image/Metro2033_foto3.jpg"
+                 alt="Third slide">
         </div>
+        <!--/Third slide-->
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
+    <!--/.Slides-->
+    <!--Controls-->
+    <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
+    <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
+    <!--/.Controls-->
 </div>
-</div>
-
 </body>
 
 
